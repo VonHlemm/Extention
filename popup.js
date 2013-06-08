bf7f20b6ff32bc32a0568a70a42061b6f3b306bf
Árni Reynir <arni.reynir89@gmail.com>
2013-06-08 21:14:00 +0000

    Friday logic

--- popup.js
+++ popup.js
@@ -1,5 +1,33 @@
 //Authors: Árni Reynir Óskarsson and Brynjólfur Jón Hermannsson
 
-var d = new Date();
-var x = document.getElementById("fryday");
-x.innerHTML=d.getDay();
+var FRIDAY = 5;
+
+var fridayChecker = {
+
+  showIfFriday : function() {
+    var date = new Date();
+    var day = date.getDay();
+
+    if( FRIDAY === day ) {
+      var p = document.createElement('p');
+      var txt = document.createTextNode("Það er föstudagur")
+      p.appendChild(txt);
+      document.body.appendChild(p);
+      console.log("Friday");
+    } else {
+      var p = document.createElement('p');
+      var txt = document.createTextNode("Það EKKI föstudagur")
+      p.appendChild(txt);
+      document.body.appendChild(p);
+      console.log("Not Friday");
+    }
+  }
+
+
+}
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  fridayChecker.showIfFriday();
+});
+
+
